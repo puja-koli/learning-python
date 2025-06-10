2d0e8b24c27891c5b812081fc53554c778c0675d
puja-koli <koli.puja567@gmail.com>
2025-06-10 08:50:45 +0000

    added sort_list.py

---
+++ 16_sort_list.py
@@ -0,0 +1,15 @@
+num=[5,32,6,2,1]
+alph=['f','j','a','g','b']
+num.sort()
+alph.sort()
+print(num)
+print(alph)
+#reverse
+num.sort(reverse=True)
+alph.sort(reverse=True)
+print(num)
+print(alph)
+
+A=[1,4,2,5]
+A.reverse()
+print(A)
