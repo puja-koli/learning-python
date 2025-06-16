795ba55e75f386c3f07f9a8c194697d061be2957
puja-koli <koli.puja567@gmail.com>
2025-06-16 23:17:27 +0600

    Add files via upload

---
+++ 33_match_case_statments.py
@@ -0,0 +1,8 @@
+x=int(input("inter your value of the x :"))
+match x:
+    case 0:
+        print('YOUR NUMBER IS ZERO')
+    case 4:
+        print('YOUR NUMBER IS FOUR')
+    case _:
+        print(x)
