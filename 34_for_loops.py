0e22ff6e34b42f5fc55533a3b314e81c40d99ddc
puja-koli <koli.puja567@gmail.com>
2025-06-16 23:40:50 +0600

    for_loops

---
+++ 34_for_loops.py
@@ -0,0 +1,22 @@
+name="Bangladesh"
+for x in name:
+    print(x)
+
+li=['Blue','Green','Red']
+for i in li:
+    print(i)
+
+fruits=['apple','cherry','banana']
+for y in fruits:
+    print(y)
+    for z in y:
+        print(z)
+    
+for a in range(3):    
+    print(a)
+    
+for b in range(1,6):    
+    print(b)
+    
+for c in range(0,12,4):
+    print(c)    
