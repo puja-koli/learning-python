cac292c304d207bc5e67b7c2cea79c6455909a98
puja-koli <koli.puja567@gmail.com>
2025-06-20 23:20:25 +0600

    Add files via upload

---
+++ 41_dictionaries.py
@@ -0,0 +1,15 @@
+dic={11:'green',12:'blue',13:'red'}
+print(type(dic))
+print(dic)
+print(dic.items())
+print(dic.values())
+print(dic.keys())
+print(dic[11])
+
+for i in dic.values():
+    print(i)
+
+
+
+
+
